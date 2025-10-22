fa7a4b3b06f89ae6d8433bcf816cf089cff2ceee
dmarley10 <dmarley@umich.edu>
2025-10-22 01:48:30 -0400

    Update Player_tests.cpp

--- starter-files/Player_tests.cpp
+++ starter-files/Player_tests.cpp
@@ -1,3 +1,4 @@
+// Player Tests
 #include "Player.hpp"
 #include "unit_test_framework.hpp"
 
@@ -5,14 +6,16 @@
 
 using namespace std;
 
+// PLAYER BASIC FUNCTIONALITY ----
+
 TEST(test_player_get_name) {
-    Player * alice = Player_factory("Alice", "Simple");
+    Player* alice = Player_factory("Alice", "Simple");
     ASSERT_EQUAL("Alice", alice->get_name());
     delete alice;
 }
 
 TEST(test_add_card) {
-    Player *ryan = Player_factory("Ryan", "Simple");
+    Player* ryan = Player_factory("Ryan", "Simple");
     ryan->add_card(Card(KING, SPADES));
     ryan->add_card(Card(QUEEN, HEARTS));
     ryan->add_card(Card(TEN, HEARTS));
@@ -22,8 +25,10 @@ TEST(test_add_card) {
     delete ryan;
 }
 
+// LEAD CARD LOGIC ----
+
 TEST(test_lead_card_trump) {
-    Player *marley = Player_factory("Marley", "Simple");
+    Player* marley = Player_factory("Marley", "Simple");
     marley->add_card(Card(TEN, HEARTS));
     marley->add_card(Card(NINE, HEARTS));
     marley->add_card(Card(ACE, HEARTS));
@@ -36,7 +41,7 @@ TEST(test_lead_card_trump) {
 }
 
 TEST(test_lead_card_not_trump) {
-    Player *marley = Player_factory("Marley", "Simple");
+    Player* marley = Player_factory("Marley", "Simple");
     marley->add_card(Card(TEN, HEARTS));
     marley->add_card(Card(NINE, HEARTS));
     marley->add_card(Card(ACE, HEARTS));
@@ -44,39 +49,53 @@ TEST(test_lead_card_not_trump) {
     marley->add_card(Card(KING, HEARTS));
 
     Card leader = marley->lead_card(SPADES);
-    ASSERT_EQUAL(leader, Card(ACE, HEARTS));    
+    ASSERT_EQUAL(leader, Card(ACE, HEARTS));
     delete marley;
 }
 
-TEST(test_lead_no_trump) {
-    Player *farquad = Player_factory("Farquad", "Simple");
-    farquad->add_card(Card(NINE, CLUBS));
-    farquad->add_card(Card(TEN, DIAMONDS));
-    farquad->add_card(Card(QUEEN, HEARTS));
-    farquad->add_card(Card(KING, SPADES));
-    farquad->add_card(Card(ACE, DIAMONDS));
+TEST(test_lead_card_non_trump) {
+    Player* max = Player_factory("Max", "Simple");
+    max->add_card(Card(NINE, HEARTS));
+    max->add_card(Card(ACE, CLUBS));
+    max->add_card(Card(TEN, SPADES));
+    max->add_card(Card(KING, CLUBS));
+    max->add_card(Card(QUEEN, CLUBS));
 
-    Card led = farquad->lead_card(HEARTS);
-    ASSERT_EQUAL(led, Card(ACE, DIAMONDS));
-    delete farquad;
+    Card lead = max->lead_card(HEARTS);
+    ASSERT_EQUAL(lead, Card(ACE, CLUBS));
+    delete max;
 }
 
-TEST(test_play_card_trump) {
-    Player *squidward = Player_factory("Squidward", "Simple");
-    squidward->add_card(Card(TEN, HEARTS));
-    squidward->add_card(Card(NINE, HEARTS));
-    squidward->add_card(Card(ACE, HEARTS));
-    squidward->add_card(Card(QUEEN, HEARTS));
-    squidward->add_card(Card(KING, HEARTS));
-    Card c(JACK, HEARTS);
+TEST(test_lead_card_chooses_highest_non_trump_when_available) {
+    Player* karen = Player_factory("Karen", "Simple");
+    karen->add_card(Card(ACE, SPADES));
+    karen->add_card(Card(KING, SPADES));
+    karen->add_card(Card(QUEEN, HEARTS));
+    karen->add_card(Card(TEN, HEARTS));
+    karen->add_card(Card(NINE, CLUBS));
 
-    Card played = squidward->play_card(c, HEARTS);
-    ASSERT_EQUAL(played, Card(ACE, HEARTS)); 
-    delete squidward;
+    Card led = karen->lead_card(HEARTS);
+    ASSERT_EQUAL(led, Card(ACE, SPADES));
+    delete karen;
 }
+
+TEST(test_lead_card_all_trump_leads_strongest_trump) {
+    Player* pearl = Player_factory("Pearl", "Simple");
+    pearl->add_card(Card(NINE, DIAMONDS));
+    pearl->add_card(Card(TEN, DIAMONDS));
+    pearl->add_card(Card(QUEEN, DIAMONDS));
+    pearl->add_card(Card(KING, DIAMONDS));
+    pearl->add_card(Card(JACK, HEARTS));
+
+    Card led = pearl->lead_card(DIAMONDS);
+    ASSERT_EQUAL(led, Card(JACK, HEARTS));
+    delete pearl;
+}
+
+// PLAY CARD LOGIC ---
 
 TEST(test_play_card_non_trump) {
-    Player *squidward = Player_factory("Squidward", "Simple");
+    Player* squidward = Player_factory("Squidward", "Simple");
     squidward->add_card(Card(TEN, HEARTS));
     squidward->add_card(Card(NINE, HEARTS));
     squidward->add_card(Card(ACE, HEARTS));
@@ -85,72 +104,80 @@ TEST(test_play_card_non_trump) {
     Card c(JACK, SPADES);
 
     Card played = squidward->play_card(c, HEARTS);
-    ASSERT_EQUAL(played, Card(NINE, HEARTS)); 
+    ASSERT_EQUAL(played, Card(NINE, HEARTS));
     delete squidward;
 }
 
-TEST(test_make_trump_1) {
-    Player *timmy = Player_factory("Timmy", "Simple");
-    timmy->add_card(Card(NINE, HEARTS));
-    timmy->add_card(Card(TEN, CLUBS));
-    timmy->add_card(Card(KING, DIAMONDS));
-    timmy->add_card(Card(JACK, HEARTS));
-    timmy->add_card(Card(KING, SPADES));
+TEST(test_play_card_left_bower_follow) {
+    Player* andrew = Player_factory("Andrew", "Simple");
+    andrew->add_card(Card(JACK, DIAMONDS));
+    andrew->add_card(Card(JACK, HEARTS));
+    andrew->add_card(Card(ACE, SPADES));
+    andrew->add_card(Card(TEN, CLUBS));
+    andrew->add_card(Card(KING, HEARTS));
 
-    Suit order = CLUBS;
-    bool choice = timmy->make_trump(Card(NINE, HEARTS), false, 1, order);
-    ASSERT_FALSE(choice);
-    ASSERT_EQUAL(order, CLUBS);
-    delete timmy;
+    Card led(TEN, HEARTS);
+    Card played = andrew->play_card(led, HEARTS);
+    ASSERT_EQUAL(played, Card(JACK, HEARTS));
+    delete andrew;
 }
 
-TEST(test_make_trump_2) {
-    Player *timmy = Player_factory("Timmy", "Simple");
-    timmy->add_card(Card(NINE, HEARTS));
-    timmy->add_card(Card(TEN, CLUBS));
-    timmy->add_card(Card(KING, DIAMONDS));
-    timmy->add_card(Card(JACK, DIAMONDS));
-    timmy->add_card(Card(KING, SPADES));
+TEST(test_play_card_left_bower) {
+    Player* larry = Player_factory("Larry", "Simple");
+    larry->add_card(Card(NINE, HEARTS));
+    larry->add_card(Card(ACE, CLUBS));
+    larry->add_card(Card(TEN, SPADES));
+    larry->add_card(Card(KING, DIAMONDS));
+    larry->add_card(Card(QUEEN, CLUBS));
 
-    Suit order = CLUBS;
-    bool choice = timmy->make_trump(Card(NINE, HEARTS), false, 1, order);
-    ASSERT_FALSE(choice);
-    ASSERT_EQUAL(order, CLUBS);
-    delete timmy;
+    Card led(JACK, DIAMONDS);
+    Card play = larry->play_card(led, HEARTS);
+    ASSERT_EQUAL(play, Card(NINE, HEARTS));
+    delete larry;
 }
 
-TEST(test_make_trump_3) {
-    Player *jimmy = Player_factory("Jimmy", "Simple");
-    jimmy->add_card(Card(NINE, HEARTS));
-    jimmy->add_card(Card(TEN, CLUBS));
-    jimmy->add_card(Card(KING, DIAMONDS));
-    jimmy->add_card(Card(JACK, DIAMONDS));
-    jimmy->add_card(Card(KING, SPADES));
+TEST(test_play_card_must_follow) {
+    Player* puff = Player_factory("Puff", "Simple");
+    puff->add_card(Card(NINE, SPADES));
+    puff->add_card(Card(QUEEN, SPADES));
+    puff->add_card(Card(KING, SPADES));
+    puff->add_card(Card(TEN, CLUBS));
+    puff->add_card(Card(NINE, HEARTS));
 
-    Suit order = CLUBS;
-    bool choice = jimmy->make_trump(Card(NINE, HEARTS), true, 2, order);
-    ASSERT_TRUE(choice);
-    ASSERT_EQUAL(order, DIAMONDS);   
-    delete jimmy;
+    Card led(TEN, SPADES);
+    Card played = puff->play_card(led, HEARTS);
+    ASSERT_EQUAL(played, Card(KING, SPADES));
+    delete puff;
 }
 
-TEST(test_make_trump_4) {
-    Player *kimmy = Player_factory("Kimmy", "Simple");
-    kimmy->add_card(Card(NINE, HEARTS));
-    kimmy->add_card(Card(TEN, CLUBS));
-    kimmy->add_card(Card(KING, DIAMONDS));
-    kimmy->add_card(Card(JACK, DIAMONDS));
-    kimmy->add_card(Card(KING, DIAMONDS));
+TEST(test_play_card_cannot_follow) {
+    Player* gary = Player_factory("Gary", "Simple");
+    gary->add_card(Card(NINE, CLUBS));
+    gary->add_card(Card(TEN, DIAMONDS));
+    gary->add_card(Card(QUEEN, CLUBS));
+    gary->add_card(Card(TEN, SPADES));
+    gary->add_card(Card(ACE, SPADES));
 
-    Suit order = CLUBS;
-    bool choice = kimmy->make_trump(Card(NINE, HEARTS), false, 2, order);
-    ASSERT_TRUE(choice);
-    ASSERT_EQUAL(order, DIAMONDS);   
-    delete kimmy;
+    Card led(KING, HEARTS);
+    Card played = gary->play_card(led, SPADES);
+    ASSERT_EQUAL(played, Card(NINE, CLUBS));
+    delete gary;
 }
 
-TEST(test_make_trump_1_pass) {
-    Player *spongebob = Player_factory("Spongebob", "Simple");
+TEST(test_play_card_one_in_hand) {
+    Player* goofy = Player_factory("Goofy", "Simple");
+    goofy->add_card(Card(ACE, SPADES));
+
+    Card led(TEN, HEARTS);
+    Card played = goofy->play_card(led, HEARTS);
+    ASSERT_EQUAL(played, Card(ACE, SPADES));
+    delete goofy;
+}
+
+// MAKE TRUMP LOGIC ---
+
+TEST(test_make_trump_round1_pass) {
+    Player* spongebob = Player_factory("Spongebob", "Simple");
     spongebob->add_card(Card(TEN, CLUBS));
     spongebob->add_card(Card(NINE, DIAMONDS));
     spongebob->add_card(Card(JACK, HEARTS));
@@ -164,129 +191,85 @@ TEST(test_make_trump_1_pass) {
     delete spongebob;
 }
 
-TEST(test_make_trump_2_pass) {
-    Player *patrick = Player_factory("Patrick", "Simple");
-    patrick->add_card(Card(NINE, HEARTS));
-    patrick->add_card(Card(TEN, CLUBS));
-    patrick->add_card(Card(KING, SPADES));
-    patrick->add_card(Card(QUEEN, SPADES));
-    patrick->add_card(Card(JACK, CLUBS));
-
-    Suit order = SPADES;
-    bool choice = patrick->make_trump(Card(NINE, HEARTS), false, 2, order);
-    ASSERT_FALSE(choice);
-    ASSERT_EQUAL(order, SPADES);
-    delete patrick;
-}
-
-TEST(test_make_trump_second_round_should_call) {
-    Player *scooby = Player_factory("Scooby", "Simple");
-    scooby->add_card(Card(NINE, SPADES));
-    scooby->add_card(Card(JACK, SPADES));
-    scooby->add_card(Card(TEN, HEARTS));
-    scooby->add_card(Card(QUEEN, HEARTS));
-    scooby->add_card(Card(KING, HEARTS));
-
-    Suit order = SPADES;
-    bool choice = scooby->make_trump(Card(TEN, CLUBS), false, 2, order);
-
-    ASSERT_TRUE(choice);
-    ASSERT_EQUAL(order, SPADES);
-    delete scooby;
-}
-
-TEST(test_screw_the_dealer) {
-    Player *shrek = Player_factory("Shrek", "Simple");
-    shrek->add_card(Card(NINE, CLUBS));
-    shrek->add_card(Card(QUEEN, HEARTS));
-    shrek->add_card(Card(KING, SPADES));
-    shrek->add_card(Card(TEN, DIAMONDS));
-    shrek->add_card(Card(JACK, DIAMONDS));
-    
-    Card upcard(JACK, HEARTS);
-    Suit order = SPADES;
-
-    bool choice = shrek->make_trump(upcard, true, 2, order);
-    ASSERT_TRUE(choice);
-    ASSERT_NOT_EQUAL(order, upcard.get_suit());
-
-    delete shrek;
-}
-
-//when Simple player should order up bc have right bower
-TEST(make_trump_first_round) {
-    Player *donkey = Player_factory("Donkey", "Simple");
-    donkey->add_card(Card(JACK, HEARTS));
-    donkey->add_card(Card(KING, DIAMONDS));
-    donkey->add_card(Card(QUEEN, SPADES));
-    donkey->add_card(Card(TEN, CLUBS));
-    donkey->add_card(Card(ACE, SPADES));
+TEST(test_make_trump_round2_order) {
+    Player* plankton = Player_factory("Plankton", "Simple");
+    plankton->add_card(Card(JACK, HEARTS));
+    plankton->add_card(Card(NINE, SPADES));
+    plankton->add_card(Card(TEN, CLUBS));
+    plankton->add_card(Card(KING, SPADES));
+    plankton->add_card(Card(NINE, DIAMONDS));
 
     Suit order = CLUBS;
-    bool choice = donkey->make_trump(Card(NINE, HEARTS), false, 1, order);
-
-    ASSERT_FALSE(choice);
-    ASSERT_EQUAL(order, CLUBS);
-    delete donkey;
+    bool choice = plankton->make_trump(Card(NINE, HEARTS), false, 2, order);
+    ASSERT_TRUE(choice);
+    ASSERT_EQUAL(order, DIAMONDS);
+    delete plankton;
 }
 
-TEST(test_play_card_follow_suit) {
-    Player *fiona = Player_factory("Fiona", "Simple");
-    fiona->add_card(Card(NINE, SPADES));
-    fiona->add_card(Card(KING, SPADES));
-    fiona->add_card(Card(QUEEN, HEARTS));
-    fiona->add_card(Card(JACK, HEARTS));
-    fiona->add_card(Card(ACE, HEARTS));
+TEST(test_make_trump_screw_dealer_round2) {
+    Player* pluto = Player_factory("Pluto", "Simple");
+    pluto->add_card(Card(NINE, HEARTS));
+    pluto->add_card(Card(TEN, CLUBS));
+    pluto->add_card(Card(NINE, DIAMONDS));
+    pluto->add_card(Card(TEN, DIAMONDS));
+    pluto->add_card(Card(NINE, SPADES));
 
-    Card lead = Card(TEN, HEARTS);
-    Card played = fiona->play_card(lead, CLUBS);
-    ASSERT_EQUAL(played, Card(ACE, HEARTS));
-    delete fiona;
+    Suit order = SPADES;
+    bool choice = pluto->make_trump(Card(NINE, HEARTS), true, 2, order);
+    ASSERT_TRUE(choice); // dealer must order up
+    ASSERT_EQUAL(order, DIAMONDS); // next suit of HEARTS
+    delete pluto;
 }
+
+// ADD AND DISCARD LOGIC ---
 
 TEST(test_add_and_discard) {
-    Player *gary = Player_factory("Gary", "Simple");
+    Player* gary = Player_factory("Gary", "Simple");
     gary->add_card(Card(TEN, HEARTS));
     gary->add_card(Card(NINE, HEARTS));
     gary->add_card(Card(QUEEN, HEARTS));
     gary->add_card(Card(KING, HEARTS));
     gary->add_card(Card(JACK, HEARTS));
 
-    gary->add_and_discard(Card(JACK, SPADES));
-
-    Card c(NINE, HEARTS);
-    bool still_has_lowest = false;
-
-    for (int i = 0; i < 5; i++) {
-        Card lead = gary->lead_card(SPADES);
-        if (lead == c) {
-            still_has_lowest = true;
-        }
-    }
-
-    ASSERT_FALSE(still_has_lowest);
+    gary->add_and_discard(Card(ACE, HEARTS));
+    Card leader = gary->lead_card(HEARTS);
+    ASSERT_EQUAL(leader, Card(JACK, HEARTS));
     delete gary;
 }
 
-TEST(test_add_and_discard_trump_replacement) {
-    Player *fred = Player_factory("Fred", "Simple");
-    fred->add_card(Card(NINE, DIAMONDS));
-    fred->add_card(Card(JACK, DIAMONDS));
-    fred->add_card(Card(QUEEN, HEARTS));
-    fred->add_card(Card(KING, CLUBS));
-    fred->add_card(Card(ACE, CLUBS));
+// CARD UTILITY / BOWER LOGIC ---
 
-    fred->add_and_discard(Card(JACK, HEARTS));
+TEST(test_card_less_bowers) {
+    Suit trump = HEARTS;
+    Card right_bower(JACK, HEARTS);
+    Card left_bower(JACK, DIAMONDS);
+    Card ace_hearts(ACE, HEARTS);
 
-    Card trump_card(JACK, HEARTS);
-    bool has_trump = false;
+    ASSERT_TRUE(Card_less(left_bower, right_bower, trump));
+    ASSERT_TRUE(Card_less(ace_hearts, right_bower, trump));
+    ASSERT_TRUE(Card_less(ace_hearts, left_bower, trump));
+}
 
-    for (int i = 0; i < 5; i++) {
-        Card trump = fred->lead_card(HEARTS);
-        if (trump == trump_card) has_trump = true; 
-    }
-    ASSERT_TRUE(has_trump);
-    delete fred;
+TEST(test_card_bower_mapping) {
+    Card jack_hearts(JACK, HEARTS);
+    Card jack_diamonds(JACK, DIAMONDS);
+    Card jack_clubs(JACK, CLUBS);
+    Card jack_spades(JACK, SPADES);
+
+    ASSERT_TRUE(jack_hearts.is_right_bower(HEARTS));
+    ASSERT_TRUE(jack_diamonds.is_left_bower(HEARTS));
+    ASSERT_TRUE(jack_diamonds.is_trump(HEARTS));
+    ASSERT_TRUE(jack_clubs.is_right_bower(CLUBS));
+    ASSERT_TRUE(jack_spades.is_left_bower(CLUBS));
+    ASSERT_TRUE(jack_spades.is_trump(CLUBS));
+}
+
+TEST(test_card_get_suit_maps_left_bower) {
+    Card jd(JACK, DIAMONDS);
+    ASSERT_EQUAL(jd.get_suit(HEARTS), HEARTS);
+    ASSERT_TRUE(jd.is_trump(HEARTS));
+    ASSERT_FALSE(jd.is_right_bower(HEARTS));
+    ASSERT_TRUE(jd.is_left_bower(HEARTS));
 }
 
 TEST_MAIN()
