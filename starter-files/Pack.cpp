4b609274b87b4ff8b6374504897c1af25d0c54f1
dmarley10 <dmarley@umich.edu>
2025-10-22 02:00:35 -0400

    Update Pack.cpp

--- starter-files/Pack.cpp
+++ starter-files/Pack.cpp
@@ -1,23 +1,24 @@
+// Pack.cpp
 #include "Pack.hpp"
 #include <algorithm>
 #include <cassert>
 #include <istream>
 using namespace std;
 
-Pack::Pack()
-    :cards{}, next(0) {
+// Initialize standard euchre deck
+Pack::Pack() : cards{}, next(0) {
     static const Rank RANKS[] = {NINE, TEN, JACK, QUEEN, KING, ACE};
     static const Suit SUITS[] = {SPADES, HEARTS, CLUBS, DIAMONDS};
 
     int i = 0;
-    for (Suit s: SUITS) {
-        for (Rank r: RANKS) {
-            cards[i] = Card(r, s);
-            i++;
+    for (Suit s : SUITS) {
+        for (Rank r : RANKS) {
+            cards[i++] = Card(r, s);
         }
     }
 }
 
+// Read 24 cards from input stream into deck
 Pack::Pack(std::istream &pack_input) : cards{}, next(0) {
     for (int i = 0; i < PACK_SIZE; i++) {
         pack_input >> cards[i];
@@ -25,28 +26,27 @@ Pack::Pack(std::istream &pack_input) : cards{}, next(0) {
 }
 
 Card Pack::deal_one() {
-    assert(next < PACK_SIZE);
+    assert(next < PACK_SIZE); // Ensure cards left to deal
     Card c = cards[next];
     next++;
-    return c;
+    return c; // Return next card and increment index
 }
 
 void Pack::reset() {
     next = 0;
 }
 
+// 7 riffle shuffles
 void Pack::shuffle() {
-    int half = PACK_SIZE / 2;
-    for(int shuff = 0; shuff < 7; shuff++) {
-        array<Card, PACK_SIZE> old = cards;
-        int out = 0;
-        for (int i = 0; i < half; i++) {
-            cards[out++] = old[half+i];
-            cards[out++] = old[i];
+    for (int k = 0; k < 7; ++k) {
+        std::array<Card, PACK_SIZE> old = cards;
+        int index = 0;
+        for (int i = 0; i < PACK_SIZE / 2; ++i) {
+            cards[index++] = old[i + PACK_SIZE / 2];  // second half first
+            cards[index++] = old[i];                  // first half next
         }
-
     }
-    next = 0;
+    next = 0; // Reset index after shuffle
 }
 
 bool Pack::empty() const {
